66f38fc2fed21d6c8291a1981c9af3eec9d4de31
Mithun Kumar MS <mithunmuddan@gmail.com>
2019-05-29 12:38:03 +0530

    uppercase

--- basic.py
+++ basic.py
@@ -14,24 +14,13 @@ def report():
 
     username=request.args.get('username')
     lower_letter=any(c.islower() for c in username)
-    upper_letter=any(cisupper() for c in username)
+    upper_letter=any(c.isupper() for c in username)
     num_end=username[-1].isdigit()
 
     report=lower_letter and upper_letter and num_end
 
     return render_template('report.html',report=report,lower=lower_letter,
                             upper=upper_letter,num_end=num_end)
-
-
-@app.route('/signup_form')
-def signup_form():
-    return render_template('signup.html')
-
-@app.route('/thank_you')
-def thank_you():
-    first= request.args.get('first')
-    last= request.args.get('last')
-    return render_template('thankyou.html',first=first,last=last)
 
 @app.errorhandler(404)
 def page_not_found(e):
