005906170b54cedf500b4ed6fe8428df8e9d891d
Mithun Kumar MS <mithunmuddan@gmail.com>
2019-05-29 12:15:43 +0530

    adding usernames

--- basic.py
+++ basic.py
@@ -5,6 +5,24 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/report')
+def report():
+    lower_letter=False
+    upper_letter=False
+    num_end=False
+
+
+    username=request.args.get('username')
+    lower_letter=any(c.islower() for c in username)
+    upper_letter=any(cisupper() for c in username)
+    num_end=username[-1].isdigit()
+
+    report=lower_letter and upper_letter and num_end
+
+    return render_template('report.html',report=report,lower=lower_letter,
+                            upper=upper_letter,num_end=num_end)
+
+
 @app.route('/signup_form')
 def signup_form():
     return render_template('signup.html')
